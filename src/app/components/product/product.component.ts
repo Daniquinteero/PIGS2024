a954580c2492842ac0b0042abe9d0607fb772ea0
NauzetDP <nauuuni@gmail.com>
2024-05-10 10:24:27 +0100

    Test of a graph in product page

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/Services/product.service';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-product',
@@ -9,13 +10,15 @@ import { ProductService } from 'src/app/Services/product.service';
 })
 export class ProductComponent implements OnInit {
   product: any;
-
+  public chart: any;
+  
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const id = params['id'];
       this.searchProduct(id);
+      this.createChart();
     });
   }
 
@@ -29,5 +32,35 @@ export class ProductComponent implements OnInit {
         console.error('Error fetching product:', error);
       }
     );
+  }
+
+  createChart(){
+  
+    this.chart = new Chart("MyChart", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+									 '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
   }
 }
